e06e838b5b2ffbb672370c1defa5fc52d7a989bf
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-18 13:14:10 -0400

    Set app.listen to port 3000

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+
+
+
+app.listen('3000' function(){
+  console.log('Node.js totally rule!!');
+});
