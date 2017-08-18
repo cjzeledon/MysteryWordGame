1b68eefec76f2b505083f94bd91037fd7eea9e8b
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-18 13:33:03 -0400

    Added index, login and signup mustache pages. Created app.set, app.get and app.engine functions.

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const session = require('express-session');
-const mustache = require('mustache-express');
+const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 
+app.engine('mustache', mustacheExpress());
+app.set ('views', './views');
+app.set ('view engine', 'mustache');
 
+app.get('/', function(req, res){
+  res.send('I am totally alive!')
+});
 
-app.listen('3000' function(){
+app.listen(3000, function(){
   console.log('Node.js totally rule!!')
 });
